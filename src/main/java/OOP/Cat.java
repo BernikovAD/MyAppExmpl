65b8e4229278a66a2b24cb2eea28d98e98cbad75
Aleksandr Bernikov <bernikov_1993@mail.ru>
2021-08-01 15:47:53 +0500

    11

--- src/main/java/OOP/Cat.java
+++ src/main/java/OOP/Cat.java
@@ -1,5 +1,7 @@
 package OOP;
 
+import java.util.Objects;
+
 public class Cat {
     String name;
     int age;
@@ -41,5 +43,16 @@ public class Cat {
         System.out.println ("Кошка уснула на " + time);
     }
 
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || this.getClass () != obj.getClass ()) return false;
+        Cat cat = (Cat) obj;
+        return age == cat.age && mass == cat.mass && Objects.equals (name, cat.name) && Objects.equals (color, cat.color);
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash (name, age, color, mass);
+    }
 }
---
+++ src/main/java/OOP/equip.java
@@ -0,0 +1,12 @@
+package OOP;
+
+public class equip {
+    String vid;
+    int price;
+
+    public equip(String vid, int price) {
+        this.vid = vid;
+        this.price = price;
+    }
+
+}
